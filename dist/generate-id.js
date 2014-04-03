5f896258a0584bc4c683050029719db5fa3ae1e7
Ben Boyle <benjamins.boyle@gmail.com>
2014-04-03 11:04:16 +1000

    distribute unminified script
    - add concat plugin
    - generate unminified script version

---
+++ dist/generate-id.js
@@ -0,0 +1,43 @@
+/*! Generate ID - v0.1.1 - 2014-04-03
+* https://github.com/bboyle/Generate-ID
+* Copyright (c) 2014 Ben Boyle; Licensed MIT */
+(function( $ ) {
+	'use strict';
+
+
+	/**
+	 * Assigns a unique value to `@id` unless hasAttribute( 'id' ) is true
+	 *
+	 * @param preferredId string to use for id value
+	 * 
+	 * @return jquery object (chaining supported)
+	 */
+	$.fn.generateId = function( preferredId ) {
+
+		var i = 1;
+
+		if ( ! preferredId ) {
+			preferredId = 'id';
+		} else {
+			preferredId = $.trim( preferredId.toLowerCase().replace( /[^a-z0-9_]+/g, ' ' )).replace( /\s+/g, '-' );
+		}
+
+		return this.each(function() {
+
+			var id;
+
+			if ( ! this.getAttribute( 'id' )) {
+
+				id = preferredId;
+				while ( document.getElementById( id )) {
+					id = preferredId + String( i );
+					i++;
+				}
+				this.setAttribute( 'id', id );
+			}
+		});
+
+	};
+
+
+}( jQuery ));
